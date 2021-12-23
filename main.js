91b5ab3e2b023e9312fb180a9f4748e39d7c9e31
Rohan-Srivastav <78367805+Rohan-Srivastav@users.noreply.github.com>
2021-12-23 11:18:57 +0530

    Update main.js

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function take_snapshot(){
 
 console.log('ml5 version:', ml5.version);
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/KrEAd9wVE/model.json',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/APjdld4LJ/model.json',modelLoaded);
 
 function modelLoaded(){
 
